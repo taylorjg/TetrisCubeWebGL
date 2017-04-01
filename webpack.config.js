23fb1655a570c2f71ec5df802295d34bcfefebfb
Jon Taylor <jont@cakesolutions.net>
2017-04-01 15:22:38 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const packageJson = require('./package.json');
+
+const serverPublic = path.join(__dirname, 'server', 'public');
+
+module.exports = {
+    entry: [
+        'babel-polyfill',
+        './dlxlib/index.js',
+        './client/index.js'
+    ],
+    output: {
+        path: serverPublic,
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { context: './client', from: '*.html' },
+            { context: './client', from: '*.css' }
+        ]),
+        new HtmlWebpackPlugin({
+            template: './client/index.html',
+            version: packageJson.version
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.glsl$/,
+                use: 'webpack-glsl-loader'
+            }
+        ]
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: serverPublic
+    }
+};
