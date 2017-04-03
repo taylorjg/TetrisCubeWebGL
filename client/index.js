eff29cd526e1d77ae9565b44ab3341773628d4e9
Jon Taylor <jont@cakesolutions.net>
2017-04-03 14:46:57 +0100

    Got very basic rendering of the completed cube working

--- client/index.js
+++ client/index.js
@@ -1,17 +1,68 @@
 import { solve } from '../solving';
+import * as SOLVING from '../solving';
+import * as THREE from 'THREE';
 
-let searchStepCount = 0;
-
-const onSearchStep = (puzzle, internalRows) => {
-    console.log(`[onSearchStep] searchStepCount: ${searchStepCount}`);
-    searchStepCount++;
+const COLOR_TABLE = {
+    [SOLVING.COLOUR_BLUE]: 'cyan',
+    [SOLVING.COLOUR_CERISE]: 'deeppink',
+    [SOLVING.COLOUR_GREEN]: 'green',
+    [SOLVING.COLOUR_MAGENTA]: 'magenta',
+    [SOLVING.COLOUR_ORANGE]: 'orange',
+    [SOLVING.COLOUR_RED]: 'orangered',
+    [SOLVING.COLOUR_YELLOW]: 'yellow'
 };
+
+const createCube = (group, color, coords) => {
+    const geometry = new THREE.CubeGeometry(1, 1, 1);
+    const cube = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({ color }));
+    cube.position.x = coords.x;
+    cube.position.y = coords.y;
+    cube.position.z = -coords.z;
+    group.add(cube);
+};
+
+const createShape = (group, color, coords) => {
+    coords.forEach(c => createCube(group, color, c));
+};
+
+const createShapeForInternalRow = (group, internalRow) => {
+    const color = COLOR_TABLE[internalRow.colour];
+    const coords = internalRow.occupiedSquares;
+    createShape(group, color, internalRow.occupiedSquares);
+};
+
+const renderInternalRows = internalRows => {
+    const group = new THREE.Group();
+    internalRows.forEach((internalRow, index) => createShapeForInternalRow(group, internalRow));
+    group.rotation.x = Math.PI / 8;
+    group.rotation.y = Math.PI / 8;
+    scene.add(group);
+    renderer.render(scene, camera);
+};
+
+const container = document.getElementById('container');
+const w = container.offsetWidth;
+const h = container.offsetHeight;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+container.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(50, w / h, 1, 40);
+camera.position.set(-1, 1.5, 10);
+scene.add(camera);
+
+const light = new THREE.DirectionalLight(0xffffff, 1.0);
+light.position.set(0, 0, 5);
+scene.add(light);
+
+renderer.render(scene, camera);
 
 const onSolutionFound = (puzzle, internalRows) => {
-    console.log(`[onSolutionFound]`);
-    internalRows.forEach((internalRow, index) =>
-        console.log(`[${index}]: ${internalRow.name}; ${JSON.stringify(internalRow.occupiedSquares)}`));
+    internalRows.forEach(internalRow =>
+        console.log(`${internalRow.name}; ${JSON.stringify(internalRow.occupiedSquares)}; ${COLOR_TABLE[internalRow.colour]}`));
+    renderInternalRows(internalRows);        
 };
 
-const solutionGenerator = solve(onSearchStep, onSolutionFound);
+const solutionGenerator = solve(null, onSolutionFound);
 solutionGenerator.next();
