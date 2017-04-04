b2a10bc1f38e28795ff3618d84043312d8f80c9b
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2017-04-04 23:12:04 +0100

    Various little tweaks + version info

--- client/index.js
+++ client/index.js
@@ -12,13 +12,29 @@ const COLOUR_TABLE = {
     [SOLVING.COLOUR_YELLOW]: 'yellow'
 };
 
-const createCube = (colour, position) => {
-    const geometry = new THREE.CubeGeometry(1, 1, 1);
-    const cube = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({
+const createMesh = colour => {
+    return new THREE.MeshLambertMaterial({
         color: colour,
         opacity: 1,
         transparent: true
-    }));
+    })    
+};
+
+const MESH_TABLE = {
+    [SOLVING.COLOUR_BLUE]: createMesh(COLOUR_TABLE[SOLVING.COLOUR_BLUE]),
+    [SOLVING.COLOUR_CERISE]: createMesh(COLOUR_TABLE[SOLVING.COLOUR_CERISE]),
+    [SOLVING.COLOUR_GREEN]: createMesh(COLOUR_TABLE[SOLVING.COLOUR_GREEN]),
+    [SOLVING.COLOUR_MAGENTA]: createMesh(COLOUR_TABLE[SOLVING.COLOUR_MAGENTA]),
+    [SOLVING.COLOUR_ORANGE]: createMesh(COLOUR_TABLE[SOLVING.COLOUR_ORANGE]),
+    [SOLVING.COLOUR_RED]: createMesh(COLOUR_TABLE[SOLVING.COLOUR_RED]),
+    [SOLVING.COLOUR_YELLOW]: createMesh(COLOUR_TABLE[SOLVING.COLOUR_YELLOW])
+};
+
+const SIZE = 0.95;
+const geometry = new THREE.CubeGeometry(SIZE, SIZE, SIZE);
+
+const createCube = (colour, position) => {
+    const cube = new THREE.Mesh(geometry, MESH_TABLE[colour]);
     cube.position.x = position.x;
     cube.position.y = position.y;
     cube.position.z = position.z;
@@ -30,8 +46,7 @@ const createCubes = (colour, positions) =>
 
 const createShapeGroup = internalRow => {
     const shapeGroup = new THREE.Group();
-    const colour = COLOUR_TABLE[internalRow.colour];
-    const cubes = createCubes(colour, internalRow.occupiedSquares);
+    const cubes = createCubes(internalRow.colour, internalRow.occupiedSquares);
     cubes.forEach(cube => shapeGroup.add(cube));
     return shapeGroup;
 };
@@ -73,8 +88,8 @@ renderer.setSize(w, h);
 container.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(31, w / h, 1, 40);
-camera.position.set(0, 2, 12);
+const camera = new THREE.PerspectiveCamera(54, w / h, 1, 40);
+camera.position.set(2, 1, 10);
 scene.add(camera);
 
 const light = new THREE.DirectionalLight(0xffffff, 1.0);
@@ -99,6 +114,12 @@ const sliderCameraY = document.getElementById('camera_y');
 const sliderCameraZ = document.getElementById('camera_z');
 const sliderCameraViewAngle = document.getElementById('camera_va');
 
+const sliderCameraXLabel = document.getElementById('camera_x_label');
+const sliderCameraYLabel = document.getElementById('camera_y_label');
+const sliderCameraZLabel = document.getElementById('camera_z_label');
+const sliderCameraViewAngleLabel = document.getElementById('camera_va_label');
+const searchStepLabel = document.getElementById('search_step');
+
 sliderCameraX.value = camera.position.x;
 sliderCameraY.value = camera.position.y;
 sliderCameraZ.value = camera.position.z;
@@ -106,19 +127,43 @@ sliderCameraViewAngle.value = camera.fov;
 
 sliderCameraX.addEventListener('change', ev => {
     updateCameraPos(pos => pos.x = Number(ev.target.value));
+    updateSliderCameraXLabel();
 });
 
 sliderCameraY.addEventListener('change', ev => {
     updateCameraPos(pos => pos.y = Number(ev.target.value));
+    updateSliderCameraYLabel();
 });
 
 sliderCameraZ.addEventListener('change', ev => {
     updateCameraPos(pos => pos.z = Number(ev.target.value));
+    updateSliderCameraZLabel();
 });
 
 sliderCameraViewAngle.addEventListener('change', ev => {
     updateCameraFov(Number(ev.target.value));
+    updateSliderCameraViewAngleLabel(ev.target.value);
 });
+
+const updateSliderCameraXLabel = () =>
+    sliderCameraXLabel.innerText = `Camera X: ${sliderCameraX.value}`;
+
+const updateSliderCameraYLabel = () =>
+    sliderCameraYLabel.innerText = `Camera Y: ${sliderCameraY.value}`;
+
+const updateSliderCameraZLabel = () =>
+    sliderCameraZLabel.innerText = `Camera Z: ${sliderCameraZ.value}`;
+
+const updateSliderCameraViewAngleLabel = () =>
+    sliderCameraViewAngleLabel.innerText = `Camera View Angle: ${sliderCameraViewAngle.value}`;
+
+const updateSearchStepLabel = count =>
+    searchStepLabel.innerText = `Search Step: ${count}`;
+
+updateSliderCameraXLabel();
+updateSliderCameraYLabel();
+updateSliderCameraZLabel();
+updateSliderCameraViewAngleLabel();
 
 const updateCameraPos = fn => {
     const pos = camera.position;
@@ -135,8 +180,11 @@ const updateCameraFov = fov => {
     render();
 };
 
+let searchStep = 0;
+
 const onQueueTimer = () => {
     if (queue.length) {
+        updateSearchStepLabel(searchStep++);
         const pairs = queue.shift();
         renderPairs(pairs);
         if (pairs.final) {
@@ -146,7 +194,8 @@ const onQueueTimer = () => {
 };
 
 const queue = [];
-const queueTimer = setInterval(onQueueTimer, 1000 / 60);
+const queueTimer = setInterval(onQueueTimer, 100);
+
 
 const onSearchStep = pairs => {
     pairs.final = false;
